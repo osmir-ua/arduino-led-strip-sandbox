2d49998324aa48a5a99b6d9ba37e198dc86baa3b
Oleksii Myronchenko <osmir.goo@gmail.com>
2019-12-30 18:53:40 +0200

    pinMode() correction. formatting structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,25 @@
 #include <Arduino.h>
-#define NUM_LEDS 30
 #include "FastLED.h"
+
+#define NUM_LEDS 30
 #define PIN 2
+
 CRGB leds[NUM_LEDS];
 byte counter;
+
 void setup() {
   FastLED.addLeds<WS2811, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(50);
-  pinMode(13, OUTPUT);
+  pinMode(PIN, OUTPUT);
 }
+
 void loop() {
   for (int i = 0; i < NUM_LEDS; i++ ) {         // от 0 до первой трети
     leds[i] = CHSV(counter + i * 2, 255, 255);  // HSV. Увеличивать HUE (цвет)
     // умножение i уменьшает шаг радуги
   }
+
   counter++;        // counter меняется от 0 до 255 (тип данных byte)
   FastLED.show();
-  delay(5);         // скорость движения радуги
+  delay(1);         // скорость движения радуги
 }
