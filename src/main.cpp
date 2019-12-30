d99cfbae3dcd93def4c1e89fc8b2bec99677a0e2
Oleksii Myronchenko <osmir.goo@gmail.com>
2019-12-30 20:42:34 +0200

    s1

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,30 @@
 #define PIN 2
 
 CRGB leds[NUM_LEDS];
-byte counter;
+byte counter = 0;
 
 void setup() {
-  FastLED.addLeds<WS2811, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.setBrightness(50);
+  FastLED.addLeds<WS2811, PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.setBrightness(10);
   pinMode(PIN, OUTPUT);
+
+  // FastLED.clearData();
 }
 
 void loop() {
+ 
+ /*
   for (int i = 0; i < NUM_LEDS; i++ ) {         // от 0 до первой трети
     leds[i] = CHSV(counter + i * 2, 255, 255);  // HSV. Увеличивать HUE (цвет)
     // умножение i уменьшает шаг радуги
   }
+*/
+ // counter++;        // counter меняется от 0 до 255 (тип данных byte)
+  // leds[counter] = CHSV(counter * 10, 127, 127);
 
-  counter++;        // counter меняется от 0 до 255 (тип данных byte)
+  leds[counter] = CRGB::Blue;
   FastLED.show();
-  delay(1);         // скорость движения радуги
+  counter++;
+
+  delay(1000);         // скорость движения радуги
 }
