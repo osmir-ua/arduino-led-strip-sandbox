a9ab4c1fcc6d0e7ac6436f7473c901d0281d7dfb
Oleksii Myronchenko <osmir.goo@gmail.com>
2019-12-30 21:33:49 +0200

    f1

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,28 @@
 #include <Arduino.h>
 #include "FastLED.h"
 
-#define NUM_LEDS 30
-#define PIN 2
+#define NUM_LEDS 90
+#define PIN 13
 
 CRGB leds[NUM_LEDS];
 byte counter = 0;
 
 void setup() {
   FastLED.addLeds<WS2811, PIN, RGB>(leds, NUM_LEDS);
-  // FastLED.setBrightness(10);
+ //  FastLED.setBrightness(10);
   pinMode(PIN, OUTPUT);
 
+ /* for (byte i = 0; i < NUM_LEDS ; i++){
+    leds[counter] = 
+  }
+*/
+  fill_solid(leds, NUM_LEDS, CRGB::IndianRed);
+  // setColorRGB(255,0,0);
+  FastLED.show();
   // FastLED.clearData();
 }
+
+
 
 void loop() {
  
@@ -25,10 +34,19 @@ void loop() {
 */
  // counter++;        // counter меняется от 0 до 255 (тип данных byte)
   // leds[counter] = CHSV(counter * 10, 127, 127);
-
+ /*
   leds[counter] = CRGB::Blue;
   FastLED.show();
   counter++;
 
   delay(1000);         // скорость движения радуги
+  */
+ /*
+  fill_solid(leds, NUM_LEDS, CRGB(0, 255, 0));
+  delay(1000);  
+  fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));
+  delay(1000);  
+  fill_solid(leds, NUM_LEDS, CRGB(0, 0, 255));
+  delay(1000);  
+*/
 }
