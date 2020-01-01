529d2e69fb7ddedbf464f57035ac3f0435174ad4
Oleksii Myronchenko <osmir.goo@gmail.com>
2020-01-01 13:29:31 +0200

    гггг

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,25 @@
 CRGB leds[NUM_LEDS];
 byte counter;
 
+long randNum;
+
 void setup() {
   FastLED.addLeds<WS2811, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(50);
   pinMode(PIN, OUTPUT);
+  //Serial.begin(9600);
 }
 
 void loop() {
   for (int i = 0; i < NUM_LEDS; i++ ) {         // от 0 до первой трети
-    leds[i] = CHSV(counter + i * 2, 255, 255);  // HSV. Увеличивать HUE (цвет)
+    //randNum = random(85,101);
+  //  Serial.println(randNum);
+
+    leds[i] = CHSV(counter + i * (255/NUM_LEDS), 255, 127 /** randNum*/);  // HSV. Увеличивать HUE (цвет)
     // умножение i уменьшает шаг радуги
   }
 
   counter++;        // counter меняется от 0 до 255 (тип данных byte)
   FastLED.show();
-  delay(10);         // скорость движения радуги
+  delay(30);         // скорость движения радуги
 }
