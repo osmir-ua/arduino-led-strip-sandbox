c79c73685eb2a59716e80a9e92ed36a37d32aece
Oleksii Myronchenko <osmir-ua@users.noreply.github.com>
2020-01-02 14:51:53 +0200

    fill leds array and print debug messages

--- src/main.cpp
+++ src/main.cpp
@@ -20,23 +20,28 @@ void setup() {
   pinMode(PIN, OUTPUT);
 
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial){;}
   Serial.println("Serial port initialised\n------------------------");
 
-  
   // timer_init_ISR_1KHz(TIMER_DEFAULT);
 
   for (int i = 0; i < NUM_LEDS; i++ ) {         // от 0 до первой трети
-    //randNum = random(85,101);
-  //  Serial.println(randNum);
+    leds[i] = CHSV(i * (255/NUM_LEDS), currentSATURATION, currentLIGHTNESS /** randNum*/);  // HSV. Увеличивать HUE (цвет)
 
-    leds[i] = CHSV(counter + i * (255/NUM_LEDS), currentSATURATION, currentLIGHTNESS /** randNum*/);  // HSV. Увеличивать HUE (цвет)
-    currentSATURATION = basicSATURATION;
-    currentLIGHTNESS = basicLIGHTNES;
+    Serial.print(i); Serial.print(".\t RGB = ("); 
+      Serial.print(leds[i].r); Serial.print(", ");
+      Serial.print(leds[i].g); Serial.print(", ");
+      Serial.print(leds[i].b); Serial.println(")");
+
+    // currentSATURATION = basicSATURATION;
+    // currentLIGHTNESS = basicLIGHTNES;
   }
 
+  Serial.println("done");
 
 }
+
+void loop() {}
 
 void timer_handle_interrupts(int timer) {
   if (timer == TIMER_DEFAULT) {  
